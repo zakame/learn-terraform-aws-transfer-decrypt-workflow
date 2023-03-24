5def8410d18979d593e8f8563afeb8091d324ff8
Zak B. Elep <zakame@zakame.net>
2023-03-24 14:24:21 +0800

    variables.tf: add PGP secret store
    Accept PGP private key and its passphrase.

--- variables.tf
+++ variables.tf
@@ -6,3 +6,15 @@ variable "region" {
 variable "transfer_user_key" {
   description = "Public SSH key for Transfer user"
 }
+
+variable "transfer_pgp_secret" {
+  default = {
+    PGPPrivateKey = "foo"
+    PGPPassphrase = "bar"
+  }
+  description = "PGP Private key and passphrase for decrypt workflow"
+
+  type      = map(string)
+  sensitive = true
+}
+  
