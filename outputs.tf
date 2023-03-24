cc458b37bea061bea267f71f031837fff5e540c9
Zak B. Elep <zakame@zakame.net>
2023-03-24 17:00:10 +0800

    outputs.tf: include Transfer server user in output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "transfer_hostname" {
   description = "AWS Transfer Server hostname"
   value       = aws_transfer_server.test.endpoint
 }
+
+output "transfer_user" {
+  description = "AWS Transfer Server user"
+  value       = aws_transfer_user.transfer_user.user_name
+}
